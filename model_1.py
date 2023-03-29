3a1510c0a81964c70bf39cd6fe166572bf15ebb3
Wenhao LUO <wenhao.luo4@etu.univ-lorraine.fr>
2023-03-29 12:17:32 -0400

    update code so it can handle every file in the ./data dir

--- model_1.py
+++ model_1.py
@@ -1,13 +1,25 @@
+import os
 import numpy as np
 import tensorflow as tf
 
 from mid_processing import DataGenerator1, MidProcess
 
-mp = MidProcess('Fugue1.mid')
-out = mp.parse()
+data = list()
+for root, dirs, files in os.walk("./data"):
+    for filename in files:
+        file_path = os.path.join(root, filename)
+
+        print(f"parsing file {filename}")
+        mp = MidProcess(file_path)
+        data.extend(mp.parse())
+    print("flag")
+
+
+# mp = MidProcess('Fugue1.mid')
+# out = mp.parse()
 dg = DataGenerator1()
-train, label = dg.generate(out)
-test, t_label = dg.generate(out)
+train, label = dg.generate(data)
+test, t_label = dg.generate(mp.parse())
 
 model = tf.keras.models.Sequential()
 
