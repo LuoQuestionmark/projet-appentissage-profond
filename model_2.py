f6c921917509699cdc4d627f81fd4747c736cae0
Wenhao LUO <Wenhao.Luo@telecomnancy.eu>
2023-04-28 00:12:17 -0400

    update model

---
+++ model_2.py
@@ -0,0 +1,56 @@
+from random import randint
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.python.ops.numpy_ops import np_config
+np_config.enable_numpy_behavior()
+
+data = np.load("./train_data/train_serial_data.npy")
+data = data.transpose(1, 0, 2)
+data /= 128
+
+# input format:
+# (batch_size, 20, 1)
+# 20 is the manually selected number as the size of a single segment
+# for the training, we will use the first 15 as input, the last 5 as output
+
+train_data  = data[10:,:15,:]
+train_label = data[10:,15:,:]
+
+test_data  = data[:10,:15,:]
+test_label = data[:10,15:,:]
+
+def create_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Embedding(input_dim=15, output_dim=5))
+    model.add(tf.keras.layers.LSTM(10))
+    model.add(tf.keras.layers.Dense(5))
+    model.summary()
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        # loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        loss='binary_crossentropy'
+        # metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    return model
+
+model = create_model()
+
+model.fit(train_data, train_label, validation_data=(test_data, test_label))
+
+noise = np.random.rand(1, 20, 1)
+prediction = model.predict(noise)
+
+print(noise)
+print([round(i * 128) for i in prediction[0]])
+
+motive = [[2,2,1,1,2,2,1,1,1,3,2,1,4,8,4]]
+prediction = model.predict(motive)
+print(prediction)
+print([round(i * 128) for i in prediction[0]])
+
+# outputs = model (data)
+# outputs = [round(i) for i in outputs]
+# print(outputs)
--- training_data_gen2.py
+++ training_data_gen2.py
@@ -3,8 +3,9 @@ This program generate the array data for further training using
 the midi files in the './data' dir.
 """
 
-from mid_processing import DataGenerator1, MidProcess
-from matplotlib import pyplot as plt
+from typing import Iterable
+from mid_processing import MidProcess
+# from matplotlib import pyplot as plt
 
 import os
 import numpy as np
@@ -18,9 +19,51 @@ import numpy as np
 #         mp = MidProcess(file_path)
 #         data.extend(mp.parse())
 
-mp = MidProcess('./data/Fugue3.mid')
-test_data = mp.parse()
+def downsize(val):
+    return round(val / 20)
 
-data = np.array([int(i.end_t - i.start_t) for i in test_data if i.channel==1])
+class DataGenerator2:
+    """
+    Output will be used as the input of a LSTM network, which asks the data in the shape
+    of [timesteps, batch, feature].
+    For the current version, feature=1 (length of a note);
+    and the timesteps=20, as each one is just a fragment of music score
+    """
+    def __init__(self, timesteps=20) -> None:
+        self.timesteps = timesteps
 
-print(data)
+    def generate(self, data: Iterable[int]):
+        batch = len(data) // self.timesteps
+        out = np.zeros((self.timesteps, batch, 1), dtype=np.uint8)
+        for b in range(batch):
+            out[:, b, 0] = data[b * self.timesteps:(b+1) * self.timesteps]
+
+        return out
+
+# mp = MidProcess('./data/Fugue3.mid')
+# test_data = mp.parse()
+
+# data = np.array([int(i.end_t - i.start_t) for i in test_data if i.channel==1])
+
+# print([downsize(i) for i in data])
+
+if __name__ == '__main__':
+    data = list()
+    dg = DataGenerator2()
+    for root, dirs, files in os.walk("./data"):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            print(f"parsing file {filename}")
+            mp = MidProcess(file_path)
+            local_data = mp.parse()
+            for chan in range(0, 6):
+                chan_data = [round((i.end_t - i.start_t) / 20) for i in local_data if i.channel==chan]
+                if len(chan_data) == 0: continue
+                data.append(dg.generate(chan_data))
+
+                # double data by seperate differently the data
+                data.append(dg.generate(chan_data[10:]))
+
+    training_data = np.concatenate(data, axis=1, dtype=float)
+
+    np.save('./train_data/train_serial_data', training_data)
